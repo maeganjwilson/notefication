c8e8830a82a35358dab7ef42168b037ce6d959cc
Maegan Wilson <maegan@maeganwilson.com>
2020-02-27 18:06:24 -0500

    Initial Commit

---
+++ Notefication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notefication
+//
+//  Created by Maegan Wilson on 2/27/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
