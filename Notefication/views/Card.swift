10dc50012dab9c7b938389dca125b50dbd4b9e8c
Maegan Wilson <maegan@maeganwilson.com>
2020-02-27 22:03:34 -0500

    first commit

---
+++ Notefication/views/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  Notefication
+//
+//  Created by Maegan Wilson on 2/27/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+    }
+}
---
+++ Notefication/views/NoteDetail.swift
@@ -0,0 +1,21 @@
+//
+//  NoteDetail.swift
+//  Notefication
+//
+//  Created by Maegan Wilson on 2/27/20.
+//  Copyright © 2020 Maegan Wilson. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteDetail()
+    }
+}
